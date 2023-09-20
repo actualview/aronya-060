a620ec187706b60f6f936611c58c114cae7294de
actualview <138713344+actualview@users.noreply.github.com>
2023-09-20 14:21:00 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Stego',
+            location: {
+                lat: 35.144717,
+                lng: 129.125802,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${35.144717}; longitude: ${129.125802};`);
+        model.setAttribute('gltf-model', './assets/stego.gltf');
+        model.setAttribute('rotation', '0 15 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '1.2 1.2 1.2');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
